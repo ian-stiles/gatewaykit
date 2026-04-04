3ac60656116dba102b5ed828b8d62807f699fe03
Ian <istiles@ancestry.com>
2026-04-04 10:55:59 -0600

    Load and parse the yaml file

--- src/main/java/com/tactical/backend/tactical_host/GatewayKitService.java
+++ src/main/java/com/tactical/backend/tactical_host/GatewayKitService.java
@@ -1,7 +1,13 @@
 package com.tactical.backend.tactical_host;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.tactical.backend.commons.GlobalVariables;
+import com.tactical.backend.commons.utils.ResourceUtils;
 import com.tactical.backend.model.AwsEnvironment;
+import com.tactical.backend.tactical_host.model.ConfigYaml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +15,18 @@ public class GatewayKitService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GatewayKitService.class.getSimpleName());
 
+    private final ObjectMapper mapper;
+
+    private ConfigYaml configYaml;
+
     public GatewayKitService() {
+        this.mapper = new ObjectMapper(new YAMLFactory());
+
+        // Ignore unknown fields (future-proofing)
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // Optional: accept missing fields gracefully
+        mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
     }
 
     public void start() {
@@ -27,8 +44,13 @@ public class GatewayKitService {
     }
 
     public boolean getControllerStatus() {
-        //false = running so we return it inversed
+        //false = running so we return it inverted
         return !GlobalVariables.getStopRequested();
     }
 
+    public void loadGatewayYamlFile(String filename) throws JsonProcessingException {
+        String yamlContent = ResourceUtils.getInstance().load(filename);
+
+        configYaml = mapper.readValue(yamlContent, ConfigYaml.class);
+    }
 }
